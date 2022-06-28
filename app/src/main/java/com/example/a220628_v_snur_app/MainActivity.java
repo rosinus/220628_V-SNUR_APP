d58fa54086ccc0e66f1d2fc554aa9ff7a0593b3e
HTH <hath@vigeotech.com>
2022-06-28 13:12:46 +0900

    SAFE NURI 20220628 안드로이드 네이티브 첫 생성

---
+++ app/src/main/java/com/example/a220628_v_snur_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a220628_v_snur_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
